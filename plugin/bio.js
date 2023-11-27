9eec7a5321711147e3724e80175e5537e9e39358
Hassan Chand <89654366+hassan5542@users.noreply.github.com>
2023-11-27 12:34:33 -0800

    Update bio.js

--- plugin/bio.js
+++ plugin/bio.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : FORZEN-MD
- * @author : YASIYA-OFC <https://github.com/yasiyaofc1>
- * @description : FORZEN-MD,A Multi-functional whatsapp bot.
+ * @project_name : 𝗛𝗔𝗦𝗦𝗔𝗡 𝗖𝗛𝗔𝗡𝗗
+ * @author : 𝗛𝗔𝗦𝗦𝗔𝗡 𝗖𝗛𝗔𝗡𝗗 <https://github.com/hassan5542>
+ * @description : 𝗛𝗔𝗦𝗦𝗔𝗡 𝗖𝗛𝗔𝗡𝗗,A Multi-functional whatsapp bot.
  * @version 1.0.0
  **/
 
